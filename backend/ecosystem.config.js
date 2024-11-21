c9be49ea60de5ff3492672df04380802ed9aa147
radiophysiker <bolshakovdmitry@bk.ru>
2024-11-21 23:53:39 +0300

    add ecosystem

---
+++ backend/ecosystem.config.js
@@ -0,0 +1,36 @@
+require('dotenv').config();
+
+const {
+  DATABASE_USER, DATABASE_PASSWORD, DATABASE_HOST, DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
+} = process.env;
+
+module.exports = {
+  apps: [{
+    name: 'api-service',
+    script: './dist/index.js',
+    env_env_production: {
+      NODE_ENV: 'production',
+      DATABASE_HOST,
+      DATABASE_USER,
+      DATABASE_PASSWORD,
+    },
+    env_development: {
+      NODE_ENV: 'development',
+      DATABASE_HOST: 'localhost',
+      DATABASE_USER: 'test-user',
+      DATABASE_PASSWORD: 'test-user-password',
+    },
+  }],
+
+  deploy: {
+    production: {
+      user: DEPLOY_USER,
+      host: DEPLOY_HOST,
+      ref: DEPLOY_REF,
+      repo: 'https://github.com/radiophysiker/nodejs-pm2-deploy.git',
+      path: DEPLOY_PATH,
+      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      'post-deploy': 'npm i && npm run build',
+    },
+  },
+};
