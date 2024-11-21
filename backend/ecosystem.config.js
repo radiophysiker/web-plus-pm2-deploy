f8ee8cb19baca4699f2cc29f1cbc5d2dd95b4905
radiophysiker <bolshakovdmitry@bk.ru>
2024-11-22 01:26:02 +0300

    change pre-deploy-local

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -1,19 +1,13 @@
 require('dotenv').config();
 
 const {
-  DATABASE_USER, DATABASE_PASSWORD, DATABASE_HOST, DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
+  DEPLOY_USER, DEPLOY_HOST, DEPLOY_PATH, DEPLOY_REF = 'origin/master',
 } = process.env;
 
 module.exports = {
   apps: [{
     name: 'api-service',
     script: './dist/app.js',
-    env_env_production: {
-      NODE_ENV: 'production',
-      DATABASE_HOST,
-      DATABASE_USER,
-      DATABASE_PASSWORD,
-    },
   }],
 
   deploy: {
@@ -22,8 +16,8 @@ module.exports = {
       host: DEPLOY_HOST,
       ref: 'origin/master',
       repo: 'https://github.com/radiophysiker/web-plus-pm2-deploy.git',
-      path: DEPLOY_PATH,
-      'pre-deploy': `scp ./*.env ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}`,
+      path: DEPLOY_REF,
+      'pre-deploy-local': `scp .env.production ${DEPLOY_USER}@${DEPLOY_HOST}:${DEPLOY_PATH}/source/backend`,
       'post-deploy': 'cd backend && npm i && npm run build && pm2 reload ecosystem.config.js --env production',
     },
   },
